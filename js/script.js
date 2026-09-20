a9dec0a93905f255b37a343dc3b61f64e0c34d04
abhijeetIT <173333375+abhijeetIT@users.noreply.github.com>
2026-09-20 19:57:29 +0530

    Data update

--- js/script.js
+++ js/script.js
@@ -478,8 +478,6 @@ function initTerminalTypewriter() {
   tick();
 }
 
-/* ================= Init ================= */
-
 document.addEventListener("DOMContentLoaded", () => {
   renderSkills();
   renderProjects();
